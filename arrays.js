fe35b8059c53ddebedddad9871a40705543f5a9d
Sophie Kane <skane0207@gmail.com>
2018-01-17 01:11:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,9 +5,8 @@ var chocolateBars = [ 'snickers',
 ]
 
 function addElementToBeginningOfArray(array, element) {
-
-return [element, ...array]
-  
+[element, ...array]
+return array  
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
